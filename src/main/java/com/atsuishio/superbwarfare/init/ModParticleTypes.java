fb18fdf095ebf83b85cdb34fccaf1b9929b3e941
CrawKatt <man849916@gmail.com>
2026-05-16 22:30:27 -0400

    Port ModParticleTypes

--- src/main/java/com/atsuishio/superbwarfare/init/ModParticleTypes.java
+++ src/main/java/com/atsuishio/superbwarfare/init/ModParticleTypes.java
@@ -6,30 +6,30 @@ import com.atsuishio.superbwarfare.client.particle.CannonMuzzleFlareOption;
 import com.atsuishio.superbwarfare.client.particle.CustomCloudOption;
 import com.atsuishio.superbwarfare.client.particle.CustomSmokeOption;
 import com.mojang.serialization.MapCodec;
+import net.minecraft.core.Registry;
 import net.minecraft.core.particles.ParticleOptions;
 import net.minecraft.core.particles.ParticleType;
 import net.minecraft.core.particles.SimpleParticleType;
 import net.minecraft.core.registries.BuiltInRegistries;
 import net.minecraft.network.RegistryFriendlyByteBuf;
 import net.minecraft.network.codec.StreamCodec;
-import net.neoforged.neoforge.registries.DeferredHolder;
-import net.neoforged.neoforge.registries.DeferredRegister;
 import org.jetbrains.annotations.NotNull;
 
 public class ModParticleTypes {
-    public static final DeferredRegister<ParticleType<?>> REGISTRY = DeferredRegister.create(BuiltInRegistries.PARTICLE_TYPE, Mod.MODID);
+    public static final SimpleParticleType FIRE_STAR = register("fire_star", new SimpleParticleType(true));
+    public static final SimpleParticleType RISING_SMOKE = register("rising_smoke", new SimpleParticleType(true));
+    public static final ParticleType<BulletDecalOption> BULLET_DECAL = register("bullet_decal",
+            createOptions(BulletDecalOption.CODEC, true, BulletDecalOption.STREAM_CODEC));
+    public static final ParticleType<CustomSmokeOption> CUSTOM_SMOKE = register("custom_smoke",
+            createOptions(CustomSmokeOption.CODEC, true, CustomSmokeOption.STREAM_CODEC));
+    public static final ParticleType<CannonMuzzleFlareOption> CANNON_MUZZLE_FLARE = register("cannon_muzzle_flare",
+            createOptions(CannonMuzzleFlareOption.CODEC, true, CannonMuzzleFlareOption.STREAM_CODEC));
+    public static final ParticleType<CustomCloudOption> CUSTOM_CLOUD = register("custom_cloud",
+            createOptions(CustomCloudOption.CODEC, true, CustomCloudOption.STREAM_CODEC));
 
-    public static final DeferredHolder<ParticleType<?>, SimpleParticleType> FIRE_STAR = REGISTRY.register("fire_star", () -> new SimpleParticleType(true));
-    public static final DeferredHolder<ParticleType<?>,SimpleParticleType> RISING_SMOKE = REGISTRY.register("rising_smoke", () -> new SimpleParticleType(true));
-    public static final DeferredHolder<ParticleType<?>, ParticleType<BulletDecalOption>> BULLET_DECAL = REGISTRY.register("bullet_decal",
-            () -> createOptions(BulletDecalOption.CODEC, true, BulletDecalOption.STREAM_CODEC));
-    public static final DeferredHolder<ParticleType<?>, ParticleType<CustomSmokeOption>> CUSTOM_SMOKE = REGISTRY.register("custom_smoke",
-            () -> createOptions(CustomSmokeOption.CODEC, true, CustomSmokeOption.STREAM_CODEC));
-    public static final DeferredHolder<ParticleType<?>, ParticleType<CannonMuzzleFlareOption>> CANNON_MUZZLE_FLARE = REGISTRY.register("cannon_muzzle_flare",
-            () -> createOptions(CannonMuzzleFlareOption.CODEC, true, CannonMuzzleFlareOption.STREAM_CODEC));
-
-    public static final DeferredHolder<ParticleType<?>, ParticleType<CustomCloudOption>> CUSTOM_CLOUD = REGISTRY.register("custom_cloud",
-            () -> createOptions(CustomCloudOption.CODEC, true, CustomCloudOption.STREAM_CODEC));
+    private static <T extends ParticleType<?>> T register(String name, T type) {
+        return Registry.register(BuiltInRegistries.PARTICLE_TYPE, Mod.loc(name), type);
+    }
 
     public static <T extends ParticleOptions> ParticleType<T> createOptions(MapCodec<T> codec, boolean overrideLimiter, StreamCodec<? super RegistryFriendlyByteBuf, T> streamCodec) {
         return new ParticleType<>(overrideLimiter) {
@@ -43,4 +43,3 @@ public class ModParticleTypes {
         };
     }
 }
-
