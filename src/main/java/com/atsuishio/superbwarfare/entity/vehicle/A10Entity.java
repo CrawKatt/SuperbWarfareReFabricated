1e291a061fc10b9a66d2dab27ad5a989dbdce5e0
CrawKatt <man849916@gmail.com>
2026-05-16 23:21:53 -0400

    Port A10Entity

--- src/main/java/com/atsuishio/superbwarfare/entity/vehicle/A10Entity.java
+++ src/main/java/com/atsuishio/superbwarfare/entity/vehicle/A10Entity.java
@@ -27,7 +27,7 @@ public class A10Entity extends GeoVehicleEntity {
     public void onEngine1Damaged(Vec3 pos) {
         if (level().isClientSide) {
             float random = 2 * (this.random.nextFloat() - 0.5f);
-            addRandomParticle(ModParticleTypes.FIRE_STAR.get(), pos, 0, level(), 0.25f, 5);
+            addRandomParticle(ModParticleTypes.FIRE_STAR, pos, 0, level(), 0.25f, 5);
             addRandomParticle(ParticleTypes.LARGE_SMOKE, pos, 0.5f, level(), 0.001f, 1);
             addRandomParticle(new CustomCloudOption(1f, 0.25f, 0, (int) (240 + 40 * random), 2.5f + 0.5f * random, -0.07f, true, true), pos, 0.5f, level(), 1.5f, 1);
         }
@@ -37,7 +37,7 @@ public class A10Entity extends GeoVehicleEntity {
     public void onEngine2Damaged(Vec3 pos) {
         if (level().isClientSide) {
             float random = 2 * (this.random.nextFloat() - 0.5f);
-            addRandomParticle(ModParticleTypes.FIRE_STAR.get(), pos, 0, level(), 0.25f, 5);
+            addRandomParticle(ModParticleTypes.FIRE_STAR, pos, 0, level(), 0.25f, 5);
             addRandomParticle(ParticleTypes.LARGE_SMOKE, pos, 0.5f, level(), 0.001f, 1);
             addRandomParticle(new CustomCloudOption(1f, 0.25f, 0, (int) (240 + 40 * random), 2.5f + 0.5f * random, -0.07f, true, true), pos, 0.5f, level(), 1.5f, 1);
         }
