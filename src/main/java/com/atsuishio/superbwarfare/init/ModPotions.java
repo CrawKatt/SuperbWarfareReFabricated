78759396cace36e6d4bbcea5f7cad149746c865b
CrawKatt <man849916@gmail.com>
2026-05-16 22:24:55 -0400

    Port ModPotions

--- src/main/java/com/atsuishio/superbwarfare/init/ModPotions.java
+++ src/main/java/com/atsuishio/superbwarfare/init/ModPotions.java
@@ -1,20 +1,21 @@
 package com.atsuishio.superbwarfare.init;
 
 import com.atsuishio.superbwarfare.Mod;
+import net.minecraft.core.Registry;
 import net.minecraft.core.registries.BuiltInRegistries;
 import net.minecraft.world.effect.MobEffectInstance;
 import net.minecraft.world.item.alchemy.Potion;
-import net.neoforged.neoforge.registries.DeferredHolder;
-import net.neoforged.neoforge.registries.DeferredRegister;
 
 @SuppressWarnings("unused")
 public class ModPotions {
-    public static final DeferredRegister<Potion> POTIONS = DeferredRegister.create(BuiltInRegistries.POTION, Mod.MODID);
+    public static final Potion SHOCK = register("superbwarfare_shock",
+            new Potion(new MobEffectInstance(ModMobEffects.SHOCK, 100, 0)));
+    public static final Potion STRONG_SHOCK = register("superbwarfare_strong_shock",
+            new Potion(new MobEffectInstance(ModMobEffects.SHOCK, 100, 1)));
+    public static final Potion LONG_SHOCK = register("superbwarfare_long_shock",
+            new Potion(new MobEffectInstance(ModMobEffects.SHOCK, 400, 0)));
 
-    public static final DeferredHolder<Potion, Potion> SHOCK = POTIONS.register("superbwarfare_shock",
-            () -> new Potion(new MobEffectInstance(ModMobEffects.SHOCK, 100, 0)));
-    public static final DeferredHolder<Potion, Potion> STRONG_SHOCK = POTIONS.register("superbwarfare_strong_shock",
-            () -> new Potion(new MobEffectInstance(ModMobEffects.SHOCK, 100, 1)));
-    public static final DeferredHolder<Potion, Potion> LONG_SHOCK = POTIONS.register("superbwarfare_long_shock",
-            () -> new Potion(new MobEffectInstance(ModMobEffects.SHOCK, 400, 0)));
+    private static Potion register(String name, Potion potion) {
+        return Registry.register(BuiltInRegistries.POTION, Mod.loc(name), potion);
+    }
 }
