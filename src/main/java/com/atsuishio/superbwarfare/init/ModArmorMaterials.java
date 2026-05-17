a8e164e98c4c46903decc0871da25282a7434bfc
CrawKatt <man849916@gmail.com>
2026-05-16 22:09:46 -0400

    Port ModArmorMaterials

--- src/main/java/com/atsuishio/superbwarfare/init/ModArmorMaterials.java
+++ src/main/java/com/atsuishio/superbwarfare/init/ModArmorMaterials.java
@@ -2,22 +2,19 @@ package com.atsuishio.superbwarfare.init;
 
 import com.atsuishio.superbwarfare.Mod;
 import net.minecraft.Util;
+import net.minecraft.core.Registry;
 import net.minecraft.core.registries.BuiltInRegistries;
 import net.minecraft.sounds.SoundEvents;
 import net.minecraft.world.item.ArmorItem;
 import net.minecraft.world.item.ArmorMaterial;
 import net.minecraft.world.item.crafting.Ingredient;
-import net.neoforged.neoforge.registries.DeferredHolder;
-import net.neoforged.neoforge.registries.DeferredRegister;
 
 import java.util.EnumMap;
 import java.util.List;
 
 public class ModArmorMaterials {
 
-    public static final DeferredRegister<ArmorMaterial> MATERIALS = DeferredRegister.create(BuiltInRegistries.ARMOR_MATERIAL, Mod.MODID);
-
-    public static final DeferredHolder<ArmorMaterial, ArmorMaterial> CEMENTED_CARBIDE = MATERIALS.register("cemented_carbide", () -> new ArmorMaterial(
+    public static final ArmorMaterial CEMENTED_CARBIDE = register("cemented_carbide", new ArmorMaterial(
             Util.make(new EnumMap<>(ArmorItem.Type.class), p -> {
                 p.put(ArmorItem.Type.BOOTS, 3);
                 p.put(ArmorItem.Type.LEGGINGS, 6);
@@ -26,12 +23,12 @@ public class ModArmorMaterials {
             }),
             10,
             SoundEvents.ARMOR_EQUIP_IRON,
-            () -> Ingredient.of(ModItems.CEMENTED_CARBIDE_INGOT.value()),
+            () -> Ingredient.of(ModItems.CEMENTED_CARBIDE_INGOT),
             List.of(),
             4F, 0.05F)
     );
 
-    public static final DeferredHolder<ArmorMaterial, ArmorMaterial> STEEL = MATERIALS.register("steel", () -> new ArmorMaterial(
+    public static final ArmorMaterial STEEL = register("steel", new ArmorMaterial(
             Util.make(new EnumMap<>(ArmorItem.Type.class), p -> {
                 p.put(ArmorItem.Type.BOOTS, 2);
                 p.put(ArmorItem.Type.LEGGINGS, 5);
@@ -40,8 +37,12 @@ public class ModArmorMaterials {
             }),
             9,
             SoundEvents.ARMOR_EQUIP_IRON,
-            () -> Ingredient.of(ModItems.STEEL_INGOT.value()),
+            () -> Ingredient.of(ModItems.STEEL_INGOT),
             List.of(),
             1F, 0)
     );
+
+    private static ArmorMaterial register(String name, ArmorMaterial material) {
+        return Registry.register(BuiltInRegistries.ARMOR_MATERIAL, Mod.loc(name), material);
+    }
 }
