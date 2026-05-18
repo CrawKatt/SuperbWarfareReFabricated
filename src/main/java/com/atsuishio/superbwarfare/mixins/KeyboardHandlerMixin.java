485d961a2b58512333346e6140777641b8544a82
CrawKatt <man849916@gmail.com>
2026-05-18 01:40:50 -0400

    Fix unregistered keybindings

---
+++ src/main/java/com/atsuishio/superbwarfare/mixins/KeyboardHandlerMixin.java
@@ -0,0 +1,17 @@
+package com.atsuishio.superbwarfare.mixins;
+
+import com.atsuishio.superbwarfare.client.ClickHandler;
+import net.minecraft.client.KeyboardHandler;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(KeyboardHandler.class)
+public class KeyboardHandlerMixin {
+
+    @Inject(method = "keyPress(JIIII)V", at = @At("HEAD"))
+    private void superbwarfare$onKeyPressed(long window, int keyCode, int scanCode, int action, int modifiers, CallbackInfo ci) {
+        ClickHandler.onKeyPressed(keyCode, scanCode, action, modifiers);
+    }
+}
