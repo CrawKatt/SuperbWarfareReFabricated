aced0444a598ce2b788edefe2447fd31cd1782f5
CrawKatt <man849916@gmail.com>
2026-05-20 15:42:17 -0400

    Fix Guns and Vehicles Vanilla Crosshair

--- src/main/java/com/atsuishio/superbwarfare/mixins/GuiMixin.java
+++ src/main/java/com/atsuishio/superbwarfare/mixins/GuiMixin.java
@@ -1,10 +1,12 @@
 package com.atsuishio.superbwarfare.mixins;
 
 import com.atsuishio.superbwarfare.client.VehicleClientRenderState;
+import com.atsuishio.superbwarfare.item.gun.GunItem;
 import net.minecraft.client.DeltaTracker;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.Gui;
 import net.minecraft.client.gui.GuiGraphics;
+import net.minecraft.client.player.LocalPlayer;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -23,6 +25,15 @@ public class GuiMixin {
     @Inject(method = "renderSelectedItemName", at = @At("HEAD"), cancellable = true)
     private void superbWarfare$renderSelectedItemName(GuiGraphics guiGraphics, CallbackInfo ci) {
         if (VehicleClientRenderState.shouldHideHandsAndHotbar(Minecraft.getInstance().player)) {
+            ci.cancel();
+        }
+    }
+
+    @Inject(method = "renderCrosshair", at = @At("HEAD"), cancellable = true)
+    private void superbWarfare$hideCrosshair(GuiGraphics guiGraphics, DeltaTracker deltaTracker, CallbackInfo ci) {
+        LocalPlayer player = Minecraft.getInstance().player;
+        if (player == null) return;
+        if (VehicleClientRenderState.shouldHideHandsAndHotbar(player) || player.getMainHandItem().getItem() instanceof GunItem) {
             ci.cancel();
         }
     }
