6185b7d4e74caf7a6a9f815bb10373959ba6cae9
Atsuishio <842960157@qq.com>
2026-07-02 21:33:34 +0800

    优化IFF

--- src/main/kotlin/com/atsuishio/superbwarfare/client/overlay/IFFOverlay.kt
+++ src/main/kotlin/com/atsuishio/superbwarfare/client/overlay/IFFOverlay.kt
@@ -49,6 +49,7 @@ object IFFOverlay : CommonOverlay("iff") {
 
     override fun shouldRender() = super.shouldRender() && DisplayConfig.IFF_HUD.get()
 
+
     override fun RenderContext.render() {
         val level = player.level()
 
@@ -58,7 +59,14 @@ object IFFOverlay : CommonOverlay("iff") {
         CuriosApi.getCuriosInventory(player).ifPresent { c ->
             c.findFirstCurio(ModItems.IFF.get()).ifPresent { _ ->
                 // ── 友方实体（绿色）──
-                val friendlyEntities = ClientSyncedEntityHandler.getSyncedFriendlyEntities(level)
+                var friendlyEntities = ClientSyncedEntityHandler.getSyncedFriendlyEntities(level)
+                val clientEntities = SeekTool.Builder(player)
+                    .friendly()
+                    .notPlayer()
+                    .build().toList()
+
+                friendlyEntities = (friendlyEntities + clientEntities).distinctBy { it.id }
+
                 for (entity in friendlyEntities) {
                     val e = level.getEntity(entity.id) ?: entity
                     if (e !== player && e.position().canBeSeen() && e !== player.vehicle) {
