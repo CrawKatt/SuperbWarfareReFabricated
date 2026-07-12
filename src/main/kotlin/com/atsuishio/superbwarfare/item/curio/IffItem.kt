Mercurows/SuperbWarfare

继续调整同步机制，添加地图框选功能，战术地图添加对实体发射导弹功能

src/main/kotlin/com/atsuishio/superbwarfare/item/curio/IffItem.kt
@@ -1,13 +1,8 @@
 package com.atsuishio.superbwarfare.item.curio
 
-import com.atsuishio.superbwarfare.config.server.MiscConfig
-import com.atsuishio.superbwarfare.entity.vehicle.base.VehicleEntity
-import com.atsuishio.superbwarfare.init.ModItems
 import com.atsuishio.superbwarfare.network.message.receive.EntitySyncMessage
-import com.atsuishio.superbwarfare.network.message.receive.PlayerInfoSyncMessage
 import com.atsuishio.superbwarfare.tools.SeekTool
 import com.atsuishio.superbwarfare.tools.ServerSyncedEntityHandler
-import com.atsuishio.superbwarfare.tools.VectorTool
 import com.atsuishio.superbwarfare.tools.sendPacketTo
 import net.minecraft.ChatFormatting
 import net.minecraft.network.chat.Component
@@ -18,6 +13,7 @@ import net.minecraft.world.level.Level
 import net.minecraftforge.event.TickEvent
 import net.minecraftforge.eventbus.api.SubscribeEvent
 import net.minecraftforge.fml.common.Mod
+import net.minecraftforge.registries.ForgeRegistries
 import top.theillusivec4.curios.api.CuriosApi
 import top.theillusivec4.curios.api.SlotContext
 import top.theillusivec4.curios.api.type.capability.ICurioItem
@@ -41,61 +37,38 @@ open class IffItem : Item(Properties().stacksTo(1)), ICurioItem {
     @Mod.EventBusSubscriber
     companion object {
         @SubscribeEvent
-        fun onIFFItemServerTick(event: TickEvent.ServerTickEvent) {
+        fun onServerTick(event: TickEvent.ServerTickEvent) {
             if (event.phase == TickEvent.Phase.START) return
-            if (!MiscConfig.SYNC_ENTITY_OVER_RANGE.get()) return
             val server = event.server
-            if (server.tickCount % MiscConfig.SYNC_ENTITY_INTERVAL.get() != 0) return
 
-            for (level in server.allLevels) {
-                val dim = level.dimension().location()
+            for (player in server.playerList.players) {
+                if (!player.isAlive) continue
+                // 将自己注册到 ServerSyncedEntityHandler，供雷达等系统发现
+                ServerSyncedEntityHandler.register(player)
 
-                // 从 ServerSyncedEntityHandler 查询候选实体
-                val candidates = ServerSyncedEntityHandler.getEntries(dim).asSequence().mapNotNull { entry ->
-                    val entity = level.getEntity(entry.entityId) ?: return@mapNotNull null
-                    if (!SeekTool.NOT_IN_SMOKE.test(entity)) return@mapNotNull null
-                    entry to entity
-                }.toList()
-
-                val players = server.playerList.players
-                for (player in players) {
-                    if (!player.isAlive) continue
-                    CuriosApi.getCuriosInventory(player).ifPresent { c ->
-                        c.findFirstCurio(ModItems.IFF.get()).ifPresent {
-                            val list = candidates.mapNotNull { (entry, entity) ->
-                                if (!SeekTool.IS_FRIENDLY.test(player, entity)) return@mapNotNull null
-                                EntitySyncMessage.SyncedEntity(
-                                    entry.entityId, entry.entityType, entry.pos, null, entry.nbt, entry.yRot,
-                                    heightAboveGround = entry.heightAboveGround,
-                                )
-                            }.toList()
-                            sendPacketTo(player, EntitySyncMessage(dim, list, true))
-
-                            val playerList = players
-                                .asSequence()
-                                .mapNotNull {
-                                    if (level != it.level()) return@mapNotNull null
-                                    if (!SeekTool.IS_FRIENDLY.test(player, it)) return@mapNotNull null
-                                    val vehicle = it.vehicle
-                                    if (vehicle != null) {
-                                        PlayerInfoSyncMessage.SyncedPlayerInfo(
-                                            it.uuid,
-                                            if (vehicle is VehicleEntity)
-                                                VectorTool.lerpGetEntityBoundingBoxCenter(vehicle, 1f)
-                                            else it.position(),
-                                            it.displayName.string,
-                                            onVehicle = true,
-                                            it == vehicle.firstPassenger
-                                        )
-                                    } else {
-                                        PlayerInfoSyncMessage.SyncedPlayerInfo(
-                                            it.uuid, it.position(), it.displayName.string,
-                                            onVehicle = false, isDriver = false
-                                        )
-                                    }
-                                }.toList()
-                            sendPacketTo(player, PlayerInfoSyncMessage(dim, playerList))
-                        }
+                // 向所有队友同步自身位置
+                val dim = player.level().dimension().location()
+                val surfaceY = player.level().getHeight(
+                    net.minecraft.world.level.levelgen.Heightmap.Types.WORLD_SURFACE,
+                    player.blockX, player.blockZ
+                )
+                val hag = (player.y - surfaceY).coerceAtLeast(0.0)
+                val synced = EntitySyncMessage.SyncedEntity(
+                    player.id,
+                    ForgeRegistries.ENTITY_TYPES.getKey(player.type)!!,
+                    player.position(),
+                    null,
+                    player.serializeNBT(),
+                    player.yRot,
+                    heightAboveGround = hag,
+                )
+                val msg = EntitySyncMessage(dim, listOf(synced), true)
+                for (teammate in server.playerList.players) {
+                    if (teammate != player && teammate.isAlive
+                        && teammate.level().dimension() == player.level().dimension()
+                        && SeekTool.IS_FRIENDLY.test(teammate, player)
+                    ) {
+                        sendPacketTo(teammate, msg)
                     }
                 }
             }

CrawKatt/SuperbWarfareReFabricated

src/main/kotlin/com/atsuishio/superbwarfare/item/curio/IffItem.kt
@@ -1,15 +1,11 @@
 package com.atsuishio.superbwarfare.item.curio
 
-import com.atsuishio.superbwarfare.config.server.MiscConfig
-import com.atsuishio.superbwarfare.entity.vehicle.base.VehicleEntity
-import com.atsuishio.superbwarfare.init.ModItems
 import com.atsuishio.superbwarfare.network.message.receive.EntitySyncMessage
-import com.atsuishio.superbwarfare.network.message.receive.PlayerInfoSyncMessage
 import com.atsuishio.superbwarfare.tools.SeekTool
 import com.atsuishio.superbwarfare.tools.ServerSyncedEntityHandler
-import com.atsuishio.superbwarfare.tools.VectorTool
 import com.atsuishio.superbwarfare.tools.sendPacketTo
 import net.minecraft.ChatFormatting
+import net.minecraft.core.registries.BuiltInRegistries
 import net.minecraft.network.chat.Component
 import net.minecraft.world.item.Item
 import net.minecraft.world.item.ItemStack
@@ -40,61 +36,38 @@ open class IffItem : Item(Properties().stacksTo(1)), ICurioItem {
     @EventBusSubscriber
     companion object {
         @SubscribeEvent
-        fun onIFFItemServerTick(event: ServerTickEvent.Post) {
-            if (!MiscConfig.SYNC_ENTITY_OVER_RANGE.get()) return
+        fun onServerTick(event: ServerTickEvent.Post) {
             val server = event.server
-            if (server.tickCount % MiscConfig.SYNC_ENTITY_INTERVAL.get() != 0) return
 
-            for (level in server.allLevels) {
-                val dim = level.dimension().location()
+            for (player in server.playerList.players) {
+                if (!player.isAlive) continue
+                // 将自己注册到 ServerSyncedEntityHandler，供雷达等系统发现
+                ServerSyncedEntityHandler.register(player)
 
-                // 从 ServerSyncedEntityHandler 查询候选实体
-                val candidates = ServerSyncedEntityHandler.getEntries(dim).asSequence().mapNotNull { entry ->
-                    val entity = level.getEntity(entry.entityId) ?: return@mapNotNull null
-                    if (!SeekTool.NOT_IN_SMOKE.test(entity)) return@mapNotNull null
-                    entry to entity
-                }.toList()
-
-                val players = server.playerList.players
-                for (player in players) {
-                    if (!player.isAlive) continue
-                    CuriosApi.getCuriosInventory(player)
-                        .flatMap { c -> c.findFirstCurio(ModItems.IFF.get()) }
-                        .ifPresent { _ ->
-                            val list = candidates.mapNotNull { (entry, entity) ->
-                                if (!SeekTool.IS_FRIENDLY.test(player, entity)) return@mapNotNull null
-                                EntitySyncMessage.SyncedEntity(
-                                    entry.entityId, entry.entityType, entry.pos, null, entry.nbt, entry.yRot,
-                                    heightAboveGround = entry.heightAboveGround,
-                                )
-                            }.toList()
-                            sendPacketTo(player, EntitySyncMessage(dim, list, true))
-
-                            val playerList = players
-                                .asSequence()
-                                .mapNotNull {
-                                    if (level != it.level()) return@mapNotNull null
-                                    if (!SeekTool.IS_FRIENDLY.test(player, it)) return@mapNotNull null
-                                    val vehicle = it.vehicle
-                                    if (vehicle != null) {
-                                        PlayerInfoSyncMessage.SyncedPlayerInfo(
-                                            it.uuid,
-                                            if (vehicle is VehicleEntity)
-                                                VectorTool.lerpGetEntityBoundingBoxCenter(vehicle, 1f)
-                                            else it.position(),
-                                            it.displayName?.string ?: "",
-                                            onVehicle = true,
-                                            it == vehicle.firstPassenger
-                                        )
-                                    } else {
-                                        PlayerInfoSyncMessage.SyncedPlayerInfo(
-                                            it.uuid, it.position(), it.displayName?.string ?: "",
-                                            onVehicle = false, isDriver = false
-                                        )
-                                    }
-                                }.toList()
-                            sendPacketTo(player, PlayerInfoSyncMessage(dim, playerList))
-                        }
+                // 向所有队友同步自身位置
+                val dim = player.level().dimension().location()
+                val surfaceY = player.level().getHeight(
+                    net.minecraft.world.level.levelgen.Heightmap.Types.WORLD_SURFACE,
+                    player.blockX, player.blockZ
+                )
+                val hag = (player.y - surfaceY).coerceAtLeast(0.0)
+                val synced = EntitySyncMessage.SyncedEntity(
+                    player.id,
+                    BuiltInRegistries.ENTITY_TYPE.getKey(player.type),
+                    player.position(),
+                    null,
+                    player.serializeNBT(player.level().registryAccess()),
+                    player.yRot,
+                    heightAboveGround = hag,
+                )
+                val msg = EntitySyncMessage(dim, listOf(synced), true)
+                for (teammate in server.playerList.players) {
+                    if (teammate != player && teammate.isAlive
+                        && teammate.level().dimension() == player.level().dimension()
+                        && SeekTool.IS_FRIENDLY.test(teammate, player)
+                    ) {
+                        sendPacketTo(teammate, msg)
+                    }
                 }
             }
         }