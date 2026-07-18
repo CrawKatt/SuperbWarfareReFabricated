febd95def8f6ddc9fb5ec599727861fb783c9b58
17146 <1714673995@qq.com>
2026-07-18 23:39:02 +0800

    修复载具受到的爆炸伤害不正确的bug

--- src/main/kotlin/com/atsuishio/superbwarfare/event/LivingEventHandler.kt
+++ src/main/kotlin/com/atsuishio/superbwarfare/event/LivingEventHandler.kt
@@ -11,7 +11,6 @@ import com.atsuishio.superbwarfare.data.gun.GunData
 import com.atsuishio.superbwarfare.data.gun.GunProp
 import com.atsuishio.superbwarfare.data.gun.value.ReloadState
 import com.atsuishio.superbwarfare.entity.living.TargetEntity
-import com.atsuishio.superbwarfare.entity.mixin.ExplosionAccess
 import com.atsuishio.superbwarfare.entity.mixin.ICustomKnockback
 import com.atsuishio.superbwarfare.entity.vehicle.base.AutoAimableEntity
 import com.atsuishio.superbwarfare.entity.vehicle.base.VehicleEntity
@@ -733,7 +732,7 @@ object LivingEventHandler {
 
             iterator.remove()
             val explosionPos = explosion.position
-            val explosionRadius = (explosion as ExplosionAccess).`superbwarfare$getRadius`() * 2.0F
+            val explosionRadius = explosion.radius * 2.0F
             if (!entity.ignoreExplosion()) {
                 val distanceRatio = sqrt(entity.distanceToSqr(explosionPos)) / explosionRadius
                 if (distanceRatio <= 1.0) {
@@ -751,7 +750,7 @@ object LivingEventHandler {
                             Explosion.getSeenPercent(explosionPos, entity)
                         val impactStrength = (1.0 - distanceRatio) * visibilityFactor
                         val damage =
-                            (impactStrength * impactStrength + impactStrength) / 2.0 * 7.0 * explosionRadius + 1.0
+                            (impactStrength * impactStrength + impactStrength) / 2.0 * explosion.damage
 
                         entity.hurt(explosion.damageSource, damage.toFloat())
                     }
