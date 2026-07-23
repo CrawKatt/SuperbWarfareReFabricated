1dc8aec9be4f5d84d80f3b732af35966e232fcc1
17146 <1714673995@qq.com>
2026-07-23 23:57:21 +0800

    补充kh39动画

--- src/main/kotlin/com/atsuishio/superbwarfare/entity/projectile/Kh39Entity.kt
+++ src/main/kotlin/com/atsuishio/superbwarfare/entity/projectile/Kh39Entity.kt
@@ -1,7 +1,15 @@
 package com.atsuishio.superbwarfare.entity.projectile
 
+import com.atsuishio.superbwarfare.client.animation.entity.BasicProjectileAnimationInstance
 import net.minecraft.world.entity.EntityType
 import net.minecraft.world.level.Level
 
 open class Kh39Entity(type: EntityType<out Kh39Entity>, level: Level) : Agm65Entity(type, level),
-    BasicGeoProjectileEntity
+    BasicGeoProjectileEntity {
+    val anim: BasicProjectileAnimationInstance<*>? =
+        if (this.level().isClientSide) BasicProjectileAnimationInstance(this) else null
+
+    override fun getAnimationInstance(): BasicProjectileAnimationInstance<*>? {
+        return this.anim
+    }
+}
