22b22feebafbe2bb04a00d1bcf7337eb0e5d2ad7
Light_Quanta <zch22248@foxmail.com>
2026-08-26 03:16:56 +0800

    object化部分物品

--- src/main/kotlin/com/atsuishio/superbwarfare/item/gun/special/TaserV2Item.kt
+++ src/main/kotlin/com/atsuishio/superbwarfare/item/gun/special/TaserV2Item.kt
@@ -2,26 +2,20 @@ package com.atsuishio.superbwarfare.item.gun.special
 
 import com.atsuishio.superbwarfare.client.renderer.gun.GeoGunRenderer
 import com.atsuishio.superbwarfare.item.gun.GunItem
-import net.minecraft.client.renderer.BlockEntityWithoutLevelRenderer
 import net.minecraftforge.api.distmarker.Dist
 import net.minecraftforge.api.distmarker.OnlyIn
 import net.minecraftforge.client.extensions.common.IClientItemExtensions
 import java.util.function.Consumer
 
-class TaserV2Item : GunItem(Properties()) {
+object TaserV2Item : GunItem(Properties()) {
 
     @OnlyIn(Dist.CLIENT)
     override fun initializeClient(consumer: Consumer<IClientItemExtensions>) {
         super.initializeClient(consumer)
         consumer.accept(object : IClientItemExtensions {
-            private var renderer: GeoGunRenderer? = null
+            private val renderer by lazy { GeoGunRenderer() }
 
-            override fun getCustomRenderer(): BlockEntityWithoutLevelRenderer {
-                if (renderer == null) {
-                    renderer = GeoGunRenderer()
-                }
-                return renderer!!
-            }
+            override fun getCustomRenderer() = renderer
         })
     }
 }
