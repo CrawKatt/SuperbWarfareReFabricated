c227157c33aa6f2bbca7a23a5e8db90098cefde5
Atsuishio <842960157@qq.com>
2026-08-27 17:00:29 +0800

    添加格洛克17射击动画

--- src/main/kotlin/com/atsuishio/superbwarfare/event/ClientEventHandler.kt
+++ src/main/kotlin/com/atsuishio/superbwarfare/event/ClientEventHandler.kt
@@ -2596,8 +2596,7 @@ object ClientEventHandler {
 
         val gunPosX = zoom * x * (recoilHorizon * (0.5f * firePosZ)).toFloat()
         val gunPosY = zoom * y * (getBoneMoveY(firePosTimer.toFloat()) * -0.05 * (1 - 0.25 * zoomTime)).toFloat()
-        val gunPosZ =
-            zoom * z * (getBoneMoveZ(firePosTimer.toFloat()) * 0.1 + 1.1f * firePosZ).toFloat() * (1 - 0.5 * zoomTime).toFloat()
+        val gunPosZ = zoom * z * (getBoneMoveZ(firePosTimer.toFloat()) * 0.03 + 1.1f * firePosZ).toFloat() * (1 - 0.5 * zoomTime).toFloat()
 
         val gunRotX =
             zoom * rotX * (-getBoneRotX(fireRotTimer.toFloat()) * Mth.DEG_TO_RAD * 0.5f + 0.01f * firePosZ).toFloat() * gripRecoilX * recoil *
