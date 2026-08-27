bf085b4ccfbf491de91055b400a5f21d64606217
17146 <1714673995@qq.com>
2026-08-27 15:30:12 +0800

    添加GeoGunItemV2

--- src/main/kotlin/com/atsuishio/superbwarfare/item/gun/special/TaserV2Item.kt
+++ src/main/kotlin/com/atsuishio/superbwarfare/item/gun/special/TaserV2Item.kt
@@ -1,24 +1,5 @@
 package com.atsuishio.superbwarfare.item.gun.special
 
-import com.atsuishio.superbwarfare.client.renderer.gun.GeoGunRenderer
-import com.atsuishio.superbwarfare.item.gun.GunItem
-import com.atsuishio.superbwarfare.registerToModBus
-import net.neoforged.bus.api.SubscribeEvent
-import net.neoforged.neoforge.client.extensions.common.IClientItemExtensions
-import net.neoforged.neoforge.client.extensions.common.RegisterClientExtensionsEvent
+import com.atsuishio.superbwarfare.item.gun.GeoGunItemV2
 
-object TaserV2Item : GunItem(Properties()) {
-
-    init {
-        registerToModBus(this)
-    }
-
-    @SubscribeEvent
-    fun initializeClient(event: RegisterClientExtensionsEvent) {
-        event.registerItem(object : IClientItemExtensions {
-            private val renderer by lazy { GeoGunRenderer() }
-
-            override fun getCustomRenderer() = renderer
-        }, this)
-    }
-}
+object TaserV2Item : GeoGunItemV2(Properties())
