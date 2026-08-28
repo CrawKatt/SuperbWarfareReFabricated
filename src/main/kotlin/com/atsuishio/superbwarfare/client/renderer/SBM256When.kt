6095739d0cbb4bdc2a5fac941aedaf5852f51277
17146 <1714673995@qq.com>
2026-08-28 20:17:14 +0800

    添加一个一个木仓火因

---
+++ src/main/kotlin/com/atsuishio/superbwarfare/client/renderer/SBM256When.kt
@@ -0,0 +1,7 @@
+package com.atsuishio.superbwarfare.client.renderer
+
+import com.github.mcmodderanchor.simplebedrockmodel.v1.particle.runtime.ParticleEmitterInstance
+
+@Deprecated(message = "SBM 256 When", level = DeprecationLevel.ERROR)
+@Suppress("Unused", "UnusedReceiverParameter")
+fun ParticleEmitterInstance.setVariable(name: String, value: Any): Unit = TODO("256 when")
