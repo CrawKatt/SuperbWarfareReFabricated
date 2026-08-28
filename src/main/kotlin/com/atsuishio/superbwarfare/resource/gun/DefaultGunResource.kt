50b786d2584b583fabc73c9246c5a1d3764ffe6b
17146 <1714673995@qq.com>
2026-08-28 20:17:14 +0800

    添加一个一个木仓火因

--- src/main/kotlin/com/atsuishio/superbwarfare/resource/gun/DefaultGunResource.kt
+++ src/main/kotlin/com/atsuishio/superbwarfare/resource/gun/DefaultGunResource.kt
@@ -54,6 +54,9 @@ class DefaultGunResource : IDBasedData<DefaultGunResource> {
     @SerialName("FlareSize")
     var flareSize: Float = 1f
 
+    @SerialName("Smoke")
+    var smoke: SmokeConfig = SmokeConfig()
+
     @SerialName("HideCrosshairWhenZoom")
     var hideCrosshairWhenZoom: Boolean = true
 
@@ -88,6 +91,55 @@ class DefaultGunResource : IDBasedData<DefaultGunResource> {
 
         @SerialName("scale")
         var scale: SerializedVector3f = Vector3f(0f, 0f, 0f)
+    }
+
+    @Serializable
+    class SmokeConfig {
+        // 初始大小
+        @SerialName("Size")
+        var size: Float = 0.3f
+
+        // 增长量
+        @SerialName("Growth")
+        var growth: Float = 0.3f
+
+        // 持续时间
+        @SerialName("Lifetime")
+        var lifetime: Float = 0.3f
+
+        // 扩散速度
+        @SerialName("Speed")
+        var speed: Float = 0.6f
+
+        // 粒子数量
+        @SerialName("Count")
+        var count: Float = 4f
+
+        // 透明度
+        @SerialName("Opacity")
+        var opacity: Float = 1f
+
+        // 速度衰减
+        @SerialName("Drag")
+        var drag: Float = 2f
+
+        @SerialName("RandomSize")
+        var randomSize: Float = 0.4f
+
+        @SerialName("RandomGrowth")
+        var randomGrowth: Float = 0.5f
+
+        @SerialName("RandomLifetime")
+        var randomLifetime: Float = 0.3f
+
+        @SerialName("RandomSpeed")
+        var randomSpeed: Float = 0.6f
+
+        @SerialName("RandomCount")
+        var randomCount: Float = 0.3f
+
+        @SerialName("RandomOpacity")
+        var randomOpacity: Float = 0.3f
     }
 
     @Serializable
