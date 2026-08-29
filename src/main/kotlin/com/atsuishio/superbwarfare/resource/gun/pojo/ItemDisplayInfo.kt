947cc874575ec57acaaa09fcc79d55f8651c25c2
17146 <1714673995@qq.com>
2026-08-29 22:38:33 +0800

    添加空仓挂机动画，补充@JvmField

--- src/main/kotlin/com/atsuishio/superbwarfare/resource/gun/pojo/ItemDisplayInfo.kt
+++ src/main/kotlin/com/atsuishio/superbwarfare/resource/gun/pojo/ItemDisplayInfo.kt
@@ -7,12 +7,15 @@ import org.joml.Vector3f
 
 @Serializable
 class ItemDisplayInfo {
+    @JvmField
     @SerialName("translation")
     var translation: SerializedVector3f = Vector3f(0f, 0f, 0f)
 
+    @JvmField
     @SerialName("rotation")
     var rotation: SerializedVector3f = Vector3f(0f, 0f, 0f)
 
+    @JvmField
     @SerialName("scale")
     var scale: SerializedVector3f = Vector3f(0f, 0f, 0f)
 }
