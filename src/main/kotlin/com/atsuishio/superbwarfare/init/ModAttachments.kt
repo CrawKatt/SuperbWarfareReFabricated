c038f22b48e4eef9061fb1fea28e8d9f4126ff16
17146 <1714673995@qq.com>
2026-09-01 00:36:08 +0800

    尝试添加V2弹夹渲染

--- src/main/kotlin/com/atsuishio/superbwarfare/init/ModAttachments.kt
+++ src/main/kotlin/com/atsuishio/superbwarfare/init/ModAttachments.kt
@@ -13,7 +13,18 @@ object ModAttachments {
     val MAGAZINE_STANDARD: Item = register("magazine_standard")
 
     @JvmField
-    val ITEMS: List<Item> = listOf(OEM_STOCK_STANDARD, MAGAZINE_STANDARD)
+    val MAGAZINE_EXTEND: Item = register("magazine_extend")
+
+    @JvmField
+    val MAGAZINE_EXTEND_PRO: Item = register("magazine_extend_pro")
+
+    @JvmField
+    val ITEMS: List<Item> = listOf(
+        OEM_STOCK_STANDARD,
+        MAGAZINE_STANDARD,
+        MAGAZINE_EXTEND,
+        MAGAZINE_EXTEND_PRO
+    )
 
     private fun register(id: String): Item {
         return Registration.item(id, Supplier { AttachmentItem("${Mod.MODID}:$id") })
