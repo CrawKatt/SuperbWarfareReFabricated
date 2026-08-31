6c58a72603e9e254fccb3e72aaf75b7520818edf
Atsuishio <842960157@qq.com>
2026-09-01 00:18:32 +0800

    迁移马林杠杆步木仓至sbm（换弹动画待修复）

---
+++ src/main/kotlin/com/atsuishio/superbwarfare/item/gun/rifle/MarlinItem.kt
@@ -0,0 +1,22 @@
+package com.atsuishio.superbwarfare.item.gun.rifle
+
+import com.atsuishio.superbwarfare.data.gun.GunData
+import com.atsuishio.superbwarfare.item.gun.GeoGunItemV2
+import java.util.function.Consumer
+
+object MarlinItem : GeoGunItemV2(Properties()) {
+
+    override fun whenNoAmmo(data: GunData) {
+        data.closeStrike.set(true)
+    }
+
+    override fun addReloadTimeBehavior(behaviors: MutableMap<Int, Consumer<GunData>?>?) {
+        super.addReloadTimeBehavior(behaviors)
+
+        behaviors?.set(7, Consumer { data: GunData ->
+            data.closeStrike.set(
+                false
+            )
+        })
+    }
+}
