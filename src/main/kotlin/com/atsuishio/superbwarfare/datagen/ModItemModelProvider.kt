276fca248c4af853511caa30b737c788421fb906
Atsuishio <842960157@qq.com>
2026-09-02 13:05:08 +0800

    优化俄制消音器模型，添加AR消音器

--- src/main/kotlin/com/atsuishio/superbwarfare/datagen/ModItemModelProvider.kt
+++ src/main/kotlin/com/atsuishio/superbwarfare/datagen/ModItemModelProvider.kt
@@ -321,6 +321,7 @@ class ModItemModelProvider(private val output: PackOutput) : DataProvider {
         simpleItem(ModItems.MEOWLENCER)
         simpleItem(ModItems.HISSILENCER)
         simpleItem(ModItems.RU_SILENCER)
+        simpleItem(ModItems.AR_SILENCER)
         simpleItem(ModItems.MAGAZINE_EXTEND)
         simpleItem(ModItems.MAGAZINE_EXTEND_PRO)
 
