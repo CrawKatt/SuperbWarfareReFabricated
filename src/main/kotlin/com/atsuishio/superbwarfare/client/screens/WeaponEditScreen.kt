1e3c95191b63b3f72c0311296e8200140e7219b1
Atsuishio <842960157@qq.com>
2026-09-02 14:15:10 +0800

    优化改装页面

--- src/main/kotlin/com/atsuishio/superbwarfare/client/screens/WeaponEditScreen.kt
+++ src/main/kotlin/com/atsuishio/superbwarfare/client/screens/WeaponEditScreen.kt
@@ -21,6 +21,7 @@ import net.minecraft.client.gui.screens.Screen
 import net.minecraft.client.renderer.GameRenderer
 import net.minecraft.network.chat.Component
 import net.minecraft.world.item.ItemStack
+import org.lwjgl.glfw.GLFW
 import kotlin.math.min
 class WeaponEditScreen(private val stack: ItemStack) : Screen(Component.empty()) {
     override fun isPauseScreen(): Boolean {
@@ -238,6 +239,10 @@ class WeaponEditScreen(private val stack: ItemStack) : Screen(Component.empty())
     override fun keyPressed(pKeyCode: Int, pScanCode: Int, pModifiers: Int): Boolean {
         if (ModKeyMappings.EDIT_MODE.matches(pKeyCode, pScanCode)) {
             this.onClose()
+            return true
+        }
+        if (pKeyCode == GLFW.GLFW_KEY_ESCAPE && editingAttachmentType != -1) {
+            editingAttachmentType = -1
             return true
         }
         return super.keyPressed(pKeyCode, pScanCode, pModifiers)
