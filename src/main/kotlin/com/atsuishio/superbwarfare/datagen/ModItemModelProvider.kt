72fcd04cbea88990a384a8b84ce510abe1fc03dd
17146 <1714673995@qq.com>
2026-09-03 00:22:56 +0800

    添加吴音器

--- src/main/kotlin/com/atsuishio/superbwarfare/datagen/ModItemModelProvider.kt
+++ src/main/kotlin/com/atsuishio/superbwarfare/datagen/ModItemModelProvider.kt
@@ -320,6 +320,7 @@ class ModItemModelProvider(private val output: PackOutput) : DataProvider {
         // attachments
         simpleItem(ModItems.MEOWLENCER)
         simpleItem(ModItems.HISSILENCER)
+        simpleItem(ModItems.SILAOWUNCER)
         simpleItem(ModItems.RU_SILENCER)
         simpleItem(ModItems.AR_SILENCER)
         simpleItem(ModItems.HANDGUN_SILENCER)
