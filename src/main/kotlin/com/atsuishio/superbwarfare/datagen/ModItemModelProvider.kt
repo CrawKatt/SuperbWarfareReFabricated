f82d8511d07add89883f934f71f5ef75f3a9ab59
Atsuishio <842960157@qq.com>
2026-09-06 16:23:52 +0800

    添加OKP-7瞄具

--- src/main/kotlin/com/atsuishio/superbwarfare/datagen/ModItemModelProvider.kt
+++ src/main/kotlin/com/atsuishio/superbwarfare/datagen/ModItemModelProvider.kt
@@ -337,6 +337,7 @@ class ModItemModelProvider(private val output: PackOutput) : DataProvider {
         simpleItem(ModItems.OSPREY_SILENCER_TRACHELIUM)
         simpleItem(ModItems.SCOPE_COYOTE)
         simpleItem(ModItems.SCOPE_EOTECH_551)
+        simpleItem(ModItems.SCOPE_OKP_7)
         simpleItem(ModItems.SCOPE_ACOG)
         simpleItem(ModItems.SILENCER_QINGLAN)
 
