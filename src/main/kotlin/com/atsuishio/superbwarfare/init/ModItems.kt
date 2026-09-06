96f4dd7d40bd555125da33a33e985384287b3877
Atsuishio <842960157@qq.com>
2026-09-06 16:23:52 +0800

    添加OKP-7瞄具

--- src/main/kotlin/com/atsuishio/superbwarfare/init/ModItems.kt
+++ src/main/kotlin/com/atsuishio/superbwarfare/init/ModItems.kt
@@ -302,6 +302,9 @@ object ModItems {
     @JvmField val SCOPE_EOTECH_551 = registerAttachment("scope_eotech_551") {
         AttachmentItem("${Mod.MODID}:scope_eutech_551")
     }
+    @JvmField val SCOPE_OKP_7 = registerAttachment("scope_okp_7") {
+        AttachmentItem("${Mod.MODID}:scope_okp_7")
+    }
     @JvmField val SCOPE_ACOG = registerAttachment("scope_acog") {
         AttachmentItem("${Mod.MODID}:scope_acog", Rarity.RARE)
     }
