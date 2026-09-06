Mercurows/SuperbWarfare

添加无限弹药指令，补充相关文本

src/main/kotlin/com/atsuishio/superbwarfare/command/AmmoCommand.kt
@@ -1,5 +1,6 @@
 package com.atsuishio.superbwarfare.command
 
+import com.atsuishio.superbwarfare.capability.entity.InfiniteAmmoCapability
 import com.atsuishio.superbwarfare.command.builder.*
 import com.atsuishio.superbwarfare.config.server.AmmoConfig
 import com.atsuishio.superbwarfare.data.gun.Ammo
@@ -96,6 +97,52 @@ val AMMO_COMMAND = buildCommand("ammo") {
             buildAmmoLimitCommand(true)
         }
     }
+
+    "infinite" {
+        "set" {
+            entitiesArg {
+                boolArg {
+                    execute {
+                        for (entity in entities) {
+                            InfiniteAmmoCapability.modify(entity) {
+                                it.hasInfinityAmmo = boolArg
+                            }
+                        }
+
+                        if (entities.size == 1) {
+                            success {
+                                Component.translatable(
+                                    "commands.superbwarfare.ammo.infinite.set.${if (boolArg) "enabled" else "disabled"}.single",
+                                    entities.iterator().next().displayName
+                                )
+                            }
+                        } else {
+                            success {
+                                Component.translatable(
+                                    "commands.superbwarfare.ammo.infinite.set.${if (boolArg) "enabled" else "disabled"}.multiple",
+                                    entities.size
+                                )
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        "get" {
+            entityArg {
+                execute {
+                    val res = InfiniteAmmoCapability.get(entity).hasInfinityAmmo
+
+                    success {
+                        Component.translatable(
+                            "commands.superbwarfare.ammo.infinite.get", entity.displayName, res
+                        )
+                    }
+                }
+            }
+        }
+    }
 }
 
 private fun CommandNode.buildAmmoLimitCommand(isAmmoBox: Boolean) {
@@ -106,7 +153,11 @@ private fun CommandNode.buildAmmoLimitCommand(isAmmoBox: Boolean) {
                 val limit = if (isAmmoBox) type.ammoBoxLimit else type.limit
 
                 success {
-                    Component.translatable("commands.superbwarfare.ammo.limit.get", Component.translatable(type.translationKey), limit)
+                    Component.translatable(
+                        "commands.superbwarfare.ammo.limit.get",
+                        Component.translatable(type.translationKey),
+                        limit
+                    )
                 }
             }
         }
@@ -124,7 +175,11 @@ private fun CommandNode.buildAmmoLimitCommand(isAmmoBox: Boolean) {
                     config.save()
 
                     success {
-                        Component.translatable("commands.superbwarfare.ammo.limit.set", Component.translatable(type.translationKey), intArg)
+                        Component.translatable(
+                            "commands.superbwarfare.ammo.limit.set",
+                            Component.translatable(type.translationKey),
+                            intArg
+                        )
                     }
                 }
             }

src/main/kotlin/com/atsuishio/superbwarfare/item/ammo/CreativeAmmoBoxItem.kt
@@ -8,6 +8,7 @@ import com.atsuishio.superbwarfare.tools.sendPacketToTrackingThis
 import net.minecraft.ChatFormatting
 import net.minecraft.network.chat.Component
 import net.minecraft.world.InteractionHand
+import net.minecraft.world.InteractionResult
 import net.minecraft.world.InteractionResultHolder
 import net.minecraft.world.entity.Entity
 import net.minecraft.world.entity.player.Player
@@ -32,7 +33,10 @@ object CreativeAmmoBoxItem : Item(Properties().rarity(Rarity.EPIC).stacksTo(1)) 
         pIsAdvanced: TooltipFlag
     ) {
         pTooltipComponents.add(
-            Component.translatable("des.superbwarfare.creative_ammo_box").withStyle(ChatFormatting.GRAY)
+            Component.translatable("des.superbwarfare.creative_ammo_box_1").withStyle(ChatFormatting.GRAY)
+        )
+        pTooltipComponents.add(
+            Component.translatable("des.superbwarfare.creative_ammo_box_2").withStyle(ChatFormatting.GRAY)
         )
     }
 
@@ -46,7 +50,7 @@ object CreativeAmmoBoxItem : Item(Properties().rarity(Rarity.EPIC).stacksTo(1)) 
         if (!event.itemStack.`is`(this)) return
         val res = invertInfinityAmmo(event.entity, event.target)
         if (res) {
-            event.isCanceled = true
+            event.cancellationResult = InteractionResult.FAIL
         }
     }
 
@@ -60,8 +64,13 @@ object CreativeAmmoBoxItem : Item(Properties().rarity(Rarity.EPIC).stacksTo(1)) 
             it.hasInfinityAmmo = hasInfiniteAmmo
         }
 
-        // TODO message
-        player?.displayClientMessage(Component.literal(if (hasInfiniteAmmo) "+ infinity" else "- infinity"), true)
+        player?.displayClientMessage(
+            Component.translatable(
+                "des.superbwarfare.creative_ammo_box.${if (hasInfiniteAmmo) "enabled" else "disabled"}",
+                entity.displayName
+            ).withStyle(if (hasInfiniteAmmo) ChatFormatting.GREEN else ChatFormatting.RED),
+            true
+        )
 
         if (entity is Player) {
             sendPacketTo(entity, ClientInfiniteAmmoMessage(entity.id, hasInfiniteAmmo))

CrawKatt/SuperbWarfareReFabricated

src/main/kotlin/com/atsuishio/superbwarfare/command/AmmoCommand.kt
@@ -1,5 +1,6 @@
 package com.atsuishio.superbwarfare.command
 
+import com.atsuishio.superbwarfare.capability.entity.InfiniteAmmoCapability
 import com.atsuishio.superbwarfare.command.builder.*
 import com.atsuishio.superbwarfare.config.server.AmmoConfig
 import com.atsuishio.superbwarfare.data.gun.Ammo
@@ -96,6 +97,52 @@ val AMMO_COMMAND = buildCommand("ammo") {
             buildAmmoLimitCommand(true)
         }
     }
+
+    "infinite" {
+        "set" {
+            entitiesArg {
+                boolArg {
+                    execute {
+                        for (entity in entities) {
+                            InfiniteAmmoCapability.modify(entity) {
+                                it.hasInfiniteAmmo = boolArg
+                            }
+                        }
+
+                        if (entities.size == 1) {
+                            success {
+                                Component.translatable(
+                                    "commands.superbwarfare.ammo.infinite.set.${if (boolArg) "enabled" else "disabled"}.single",
+                                    entities.iterator().next().displayName
+                                )
+                            }
+                        } else {
+                            success {
+                                Component.translatable(
+                                    "commands.superbwarfare.ammo.infinite.set.${if (boolArg) "enabled" else "disabled"}.multiple",
+                                    entities.size
+                                )
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        "get" {
+            entityArg {
+                execute {
+                    val res = InfiniteAmmoCapability.get(entity).hasInfiniteAmmo
+
+                    success {
+                        Component.translatable(
+                            "commands.superbwarfare.ammo.infinite.get", entity.displayName, res
+                        )
+                    }
+                }
+            }
+        }
+    }
 }
 
 private fun CommandNode.buildAmmoLimitCommand(isAmmoBox: Boolean) {
@@ -106,7 +153,11 @@ private fun CommandNode.buildAmmoLimitCommand(isAmmoBox: Boolean) {
                 val limit = if (isAmmoBox) type.ammoBoxLimit else type.limit
 
                 success {
-                    Component.translatable("commands.superbwarfare.ammo.limit.get", Component.translatable(type.translationKey), limit)
+                    Component.translatable(
+                        "commands.superbwarfare.ammo.limit.get",
+                        Component.translatable(type.translationKey),
+                        limit
+                    )
                 }
             }
         }
@@ -124,7 +175,11 @@ private fun CommandNode.buildAmmoLimitCommand(isAmmoBox: Boolean) {
                     config.save()
 
                     success {
-                        Component.translatable("commands.superbwarfare.ammo.limit.set", Component.translatable(type.translationKey), intArg)
+                        Component.translatable(
+                            "commands.superbwarfare.ammo.limit.set",
+                            Component.translatable(type.translationKey),
+                            intArg
+                        )
                     }
                 }
             }

src/main/kotlin/com/atsuishio/superbwarfare/item/ammo/CreativeAmmoBoxItem.kt
@@ -1,6 +1,6 @@
 package com.atsuishio.superbwarfare.item.ammo
 
-import com.atsuishio.superbwarfare.capability.living.InfiniteAmmoCapability
+import com.atsuishio.superbwarfare.capability.entity.InfiniteAmmoCapability
 import com.atsuishio.superbwarfare.network.message.receive.ClientInfiniteAmmoMessage
 import com.atsuishio.superbwarfare.tools.sendPacketTo
 import com.atsuishio.superbwarfare.tools.sendPacketToTrackingThis
@@ -41,7 +41,10 @@ object CreativeAmmoBoxItem : Item(Properties().rarity(Rarity.EPIC).stacksTo(1)) 
         tooltipFlag: TooltipFlag
     ) {
         tooltipComponents.add(
-            Component.translatable("des.superbwarfare.creative_ammo_box").withStyle(ChatFormatting.GRAY)
+            Component.translatable("des.superbwarfare.creative_ammo_box_1").withStyle(ChatFormatting.GRAY)
+        )
+        tooltipComponents.add(
+            Component.translatable("des.superbwarfare.creative_ammo_box_2").withStyle(ChatFormatting.GRAY)
         )
     }
 
@@ -60,8 +63,13 @@ object CreativeAmmoBoxItem : Item(Properties().rarity(Rarity.EPIC).stacksTo(1)) 
             it.hasInfiniteAmmo = hasInfiniteAmmo
         }
 
-        // TODO message
-        player?.displayClientMessage(Component.literal(if (hasInfiniteAmmo) "+ infinity" else "- infinity"), true)
+        player?.displayClientMessage(
+            Component.translatable(
+                "des.superbwarfare.creative_ammo_box.${if (hasInfiniteAmmo) "enabled" else "disabled"}",
+                entity.displayName
+            ).withStyle(if (hasInfiniteAmmo) ChatFormatting.GREEN else ChatFormatting.RED),
+            true
+        )
 
         if (entity is Player) {
             sendPacketTo(entity, ClientInfiniteAmmoMessage(entity.id, hasInfiniteAmmo))