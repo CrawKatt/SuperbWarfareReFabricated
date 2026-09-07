Mercurows/SuperbWarfare

优化Capability序列化方法

src/main/kotlin/com/atsuishio/superbwarfare/capability/living/PhosphorusFireCapability.kt
@@ -2,36 +2,43 @@ package com.atsuishio.superbwarfare.capability.living
 
 import com.atsuishio.superbwarfare.Mod.Companion.loc
 import com.atsuishio.superbwarfare.capability.ModCapabilities
+import com.atsuishio.superbwarfare.serialization.decodeFromCompoundTag
+import com.atsuishio.superbwarfare.serialization.encodeToCompoundTag
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+import kotlinx.serialization.serializer
 import net.minecraft.nbt.CompoundTag
 import net.minecraft.resources.ResourceLocation
-import net.minecraft.world.entity.LivingEntity
+import net.minecraft.world.entity.Entity
 import net.minecraftforge.common.capabilities.AutoRegisterCapability
 import net.minecraftforge.common.util.INBTSerializable
 
 @AutoRegisterCapability
-class PhosphorusFireCapability : INBTSerializable<CompoundTag> {
-    var isOnFire: Boolean = false
+@Serializable
+data class PhosphorusFireCapability(
+    @SerialName("SbwPhosphorusFire")
+    var isOnFire: Boolean = false,
+) : INBTSerializable<CompoundTag> {
 
-    override fun serializeNBT(): CompoundTag {
-        val tag = CompoundTag()
-        tag.putBoolean(TAG_PHOSPHORUS_FIRE, this.isOnFire)
-        return tag
-    }
+    override fun serializeNBT(): CompoundTag =
+        encodeToCompoundTag(serializer<PhosphorusFireCapability>(), this)
 
     override fun deserializeNBT(nbt: CompoundTag) {
-        if (nbt.contains(TAG_PHOSPHORUS_FIRE)) {
-            this.isOnFire = nbt.getBoolean(TAG_PHOSPHORUS_FIRE)
-        }
+        isOnFire = decodeFromCompoundTag(serializer<PhosphorusFireCapability>(), nbt).isOnFire
     }
 
     companion object {
         val ID: ResourceLocation = loc("phosphorus_fire_capability")
-        const val TAG_PHOSPHORUS_FIRE: String = "SbwPhosphorusFire"
 
         @JvmStatic
-        fun of(living: LivingEntity): PhosphorusFireCapability {
-            return living.getCapability(ModCapabilities.PHOSPHORUS_FIRE_CAPABILITY)
+        fun get(entity: Entity): PhosphorusFireCapability {
+            return entity.getCapability(ModCapabilities.PHOSPHORUS_FIRE_CAPABILITY)
                 .orElseGet { PhosphorusFireCapability() }
+        }
+
+        @JvmStatic
+        fun set(entity: Entity, value: Boolean) {
+            get(entity).isOnFire = value
         }
     }
 }

CrawKatt/SuperbWarfareReFabricated

src/main/kotlin/com/atsuishio/superbwarfare/capability/living/PhosphorusFireCapability.kt
@@ -2,28 +2,41 @@ package com.atsuishio.superbwarfare.capability.living
 
 import com.atsuishio.superbwarfare.Mod.Companion.loc
 import com.atsuishio.superbwarfare.capability.ModCapabilities
+import com.atsuishio.superbwarfare.serialization.decodeFromCompoundTag
+import com.atsuishio.superbwarfare.serialization.encodeToCompoundTag
 import dev.onyxstudios.cca.api.v3.component.Component
+import net.minecraft.world.entity.LivingEntity
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+import kotlinx.serialization.serializer
 import net.minecraft.nbt.CompoundTag
 import net.minecraft.resources.ResourceLocation
-import net.minecraft.world.entity.LivingEntity
 
-class PhosphorusFireCapability : Component {
-    var isOnFire: Boolean = false
+@Serializable
+data class PhosphorusFireCapability(
+    @SerialName("SbwPhosphorusFire")
+    var isOnFire: Boolean = false,
+) : Component {
 
     override fun readFromNbt(tag: CompoundTag) {
-        if (tag.contains(TAG_PHOSPHORUS_FIRE)) isOnFire = tag.getBoolean(TAG_PHOSPHORUS_FIRE)
+        isOnFire = decodeFromCompoundTag(serializer<PhosphorusFireCapability>(), tag).isOnFire
     }
 
     override fun writeToNbt(tag: CompoundTag) {
-        tag.putBoolean(TAG_PHOSPHORUS_FIRE, isOnFire)
+        val written = encodeToCompoundTag(serializer<PhosphorusFireCapability>(), this)
+        for (key in written.allKeys) written.get(key)?.let { tag.put(key, it) }
     }
 
     companion object {
         @JvmField
         val ID: ResourceLocation = loc("phosphorus_fire_capability")
-        const val TAG_PHOSPHORUS_FIRE = "SbwPhosphorusFire"
 
         @JvmStatic
-        fun of(living: LivingEntity): PhosphorusFireCapability = ModCapabilities.PHOSPHORUS_FIRE.get(living)
+        fun get(entity: LivingEntity): PhosphorusFireCapability = ModCapabilities.PHOSPHORUS_FIRE.get(entity)
+
+        @JvmStatic
+        fun set(entity: LivingEntity, value: Boolean) {
+            get(entity).isOnFire = value
+        }
     }
 }