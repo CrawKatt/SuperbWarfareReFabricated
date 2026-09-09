0ad677dd3c4d9d2e295ba7b35a0dda1514221fff
Atsuishio <842960157@qq.com>
2026-09-09 17:00:51 +0800

    迁移HK416

--- src/main/kotlin/com/atsuishio/superbwarfare/item/gun/rifle/Hk416Item.kt
+++ src/main/kotlin/com/atsuishio/superbwarfare/item/gun/rifle/Hk416Item.kt
@@ -1,8 +1,8 @@
-package com.atsuishio.superbwarfare.item.gun.sniper
+package com.atsuishio.superbwarfare.item.gun.rifle
 
 import com.atsuishio.superbwarfare.init.RegistryName
 import com.atsuishio.superbwarfare.item.gun.GeoGunItemV2
 import net.minecraft.world.item.Rarity
 
-@RegistryName("k_98")
-object K98Item : GeoGunItemV2(Properties().rarity(Rarity.RARE))
+@RegistryName("hk_416")
+object Hk416Item : GeoGunItemV2(Properties().rarity(Rarity.RARE))
