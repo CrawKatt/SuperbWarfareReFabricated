7e8dea0c564a04c1da468f63d97add3f64752dbb
Atsuishio <842960157@qq.com>
2026-09-10 12:01:21 +0800

    添加1P78瞄准镜

--- src/main/kotlin/com/atsuishio/superbwarfare/init/ModItems.kt
+++ src/main/kotlin/com/atsuishio/superbwarfare/init/ModItems.kt
@@ -322,6 +322,9 @@ object ModItems {
     @JvmField val SCOPE_ACOG = registerAttachment("scope_acog") {
         AttachmentItem("${Mod.MODID}:scope_acog", Rarity.RARE)
     }
+    @JvmField val SCOPE_1P_78 = registerAttachment("scope_1p_78") {
+        AttachmentItem("${Mod.MODID}:scope_1p_78", Rarity.RARE)
+    }
     @JvmField val SCOPE_HAMR = registerAttachment("scope_hamr") {
         AttachmentItem("${Mod.MODID}:scope_hamr", Rarity.EPIC)
     }
