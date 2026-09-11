6f9e0fa0f0c8cfb20825b4fcff93e6bc2a19765a
Light_Quanta <zch22248@foxmail.com>
2026-09-11 15:27:02 +0800

    重命名createMapCodec

--- src/main/kotlin/com/atsuishio/superbwarfare/init/ModParticleTypes.kt
+++ src/main/kotlin/com/atsuishio/superbwarfare/init/ModParticleTypes.kt
@@ -2,8 +2,8 @@ package com.atsuishio.superbwarfare.init
 
 import com.atsuishio.superbwarfare.Mod
 import com.atsuishio.superbwarfare.client.particle.*
+import com.atsuishio.superbwarfare.tools.createMapCodec
 import com.atsuishio.superbwarfare.tools.createStreamCodec
-import com.atsuishio.superbwarfare.tools.generateMapCodec
 import com.mojang.serialization.MapCodec
 import net.minecraft.core.Registry
 import net.minecraft.core.particles.ParticleOptions
@@ -51,7 +51,7 @@ object ModParticleTypes {
     inline fun <reified T : ParticleOptions> registerParticle(
         name: String,
         overrideLimiter: Boolean = true,
-        codec: MapCodec<T> = generateMapCodec<T>(),
+        codec: MapCodec<T> = createMapCodec<T>(),
         streamCodec: StreamCodec<in RegistryFriendlyByteBuf, T> = createStreamCodec<T>(),
     ): ParticleType<T> = Registry.register(
         BuiltInRegistries.PARTICLE_TYPE,
