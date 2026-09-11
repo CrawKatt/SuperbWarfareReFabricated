54c642c06250dca0a87e8a4ad5bd685081403ca3
Atsuishio <842960157@qq.com>
2026-09-11 23:42:47 +0800

    线性膛口装置改名

--- src/main/kotlin/com/atsuishio/superbwarfare/datagen/ModItemModelProvider.kt
+++ src/main/kotlin/com/atsuishio/superbwarfare/datagen/ModItemModelProvider.kt
@@ -354,7 +354,7 @@ class ModItemModelProvider(private val output: PackOutput) : DataProvider {
         simpleItem(ModItems.SCOPE_HAMR)
         simpleItem(ModItems.SCOPE_1P_78)
         simpleItem(ModItems.TRIANGULAR_MUZZLE)
-        simpleItem(ModItems.CHOKE_MUZZLE)
+        simpleItem(ModItems.LINEAR_MUZZLE)
         simpleItem(ModItems.CROSS_MUZZLE)
         simpleItem(ModItems.STAR_MUZZLE)
         simpleItem(ModItems.LOVE_MUZZLE)
