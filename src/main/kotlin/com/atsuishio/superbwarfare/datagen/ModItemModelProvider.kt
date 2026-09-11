2f2f857ef2a9702c17b1a99bd148a9424307713a
17146 <1714673995@qq.com>
2026-09-11 22:34:18 +0800

    添加鸭嘴和交叉型配件（后者待修复模型）

--- src/main/kotlin/com/atsuishio/superbwarfare/datagen/ModItemModelProvider.kt
+++ src/main/kotlin/com/atsuishio/superbwarfare/datagen/ModItemModelProvider.kt
@@ -354,6 +354,8 @@ class ModItemModelProvider(private val output: PackOutput) : DataProvider {
         simpleItem(ModItems.SCOPE_HAMR)
         simpleItem(ModItems.SCOPE_1P_78)
         simpleItem(ModItems.TRIANGULAR_MUZZLE)
+        simpleItem(ModItems.CHOKE_MUZZLE)
+        simpleItem(ModItems.CROSS_MUZZLE)
         simpleItem(ModItems.STAR_MUZZLE)
         simpleItem(ModItems.LOVE_MUZZLE)
 
