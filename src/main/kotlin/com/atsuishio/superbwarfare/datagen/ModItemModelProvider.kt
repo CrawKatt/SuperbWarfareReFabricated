32f4b998b69438ee1da6b7241b905588423359f8
Atsuishio <842960157@qq.com>
2026-09-12 15:26:19 +0800

    添加2倍全息衍射式瞄准镜“格斗家”，添加瞄准镜弹药条和弹药数量显示功能

--- src/main/kotlin/com/atsuishio/superbwarfare/datagen/ModItemModelProvider.kt
+++ src/main/kotlin/com/atsuishio/superbwarfare/datagen/ModItemModelProvider.kt
@@ -346,6 +346,7 @@ class ModItemModelProvider(private val output: PackOutput) : DataProvider {
         simpleItem(ModItems.SCOPE_EOTECH_551)
         simpleItem(ModItems.SCOPE_OKP_7)
         simpleItem(ModItems.SCOPE_PKAS)
+        simpleItem(ModItems.SCOPE_BRUISER)
         simpleItem(ModItems.SCOPE_ACOG)
         simpleItem(ModItems.SCOPE_QMK171)
         simpleItem(ModItems.SCOPE_LPVO)
