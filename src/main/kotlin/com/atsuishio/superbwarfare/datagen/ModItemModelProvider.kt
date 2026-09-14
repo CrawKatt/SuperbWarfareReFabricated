8940d7be60bd356453c90cb1fa10a885306c323c
Atsuishio <842960157@qq.com>
2026-09-14 16:45:44 +0800

    迁移AA12

--- src/main/kotlin/com/atsuishio/superbwarfare/datagen/ModItemModelProvider.kt
+++ src/main/kotlin/com/atsuishio/superbwarfare/datagen/ModItemModelProvider.kt
@@ -37,7 +37,7 @@ class ModItemModelProvider(private val output: PackOutput) : DataProvider {
 
     private fun registerModels() {
         // gun
-        gunItem(ModItems.AA_12)
+        gunItemV2(ModItems.AA_12)
         gunItemV2(ModItems.AK_12)
         gunItemV2(ModItems.AK_47)
         gunItem(ModItems.BOCEK)
