aec9d66662f2493c6cb6d639de8ab8722014280c
Atsuishio <842960157@qq.com>
2026-09-14 16:45:44 +0800

    迁移AA12

--- src/main/kotlin/com/atsuishio/superbwarfare/init/ModItems.kt
+++ src/main/kotlin/com/atsuishio/superbwarfare/init/ModItems.kt
@@ -122,7 +122,7 @@ object ModItems {
     @JvmField val NTW_20 = registerGun("ntw_20") { Ntw20Item() }
     @JvmField val M_870 = registerGun("m_870") { M870Item }
     @JvmField val M_1897 = registerGun("m_1897") { M1987Item }
-    @JvmField val AA_12 = registerGun("aa_12") { Aa12Item() }
+    @JvmField val AA_12 = registerGun("aa_12") { Aa12Item }
     @JvmField val DEVOTION = registerGun("devotion") { DevotionItem() }
     @JvmField val RPK = registerGun("rpk") { RpkItem }
     @JvmField val M_60 = registerGun("m_60") { M60Item() }
