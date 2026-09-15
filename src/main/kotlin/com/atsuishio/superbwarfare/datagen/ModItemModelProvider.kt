f858fb8242a2297bb9773ba3144b4008953b1783
Atsuishio <842960157@qq.com>
2026-09-15 23:57:37 +0800

    添加Elcan4倍瞄准镜

--- src/main/kotlin/com/atsuishio/superbwarfare/datagen/ModItemModelProvider.kt
+++ src/main/kotlin/com/atsuishio/superbwarfare/datagen/ModItemModelProvider.kt
@@ -357,6 +357,7 @@ class ModItemModelProvider(private val output: PackOutput) : DataProvider {
         simpleItem(ModItems.SCOPE_SNIPER)
         simpleItem(ModItems.SILENCER_QINGLAN)
         simpleItem(ModItems.SCOPE_HAMR)
+        simpleItem(ModItems.SCOPE_ELCAN_4X)
         simpleItem(ModItems.SCOPE_1P_78)
         simpleItem(ModItems.SCOPE_PU)
         simpleItem(ModItems.SCOPE_PK_A)
