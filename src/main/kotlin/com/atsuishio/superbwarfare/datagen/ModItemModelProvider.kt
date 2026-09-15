864cc01477290b20119a54929dcf5fffcd81877a
Atsuishio <842960157@qq.com>
2026-09-16 00:26:17 +0800

    添加矩形垂直握把

--- src/main/kotlin/com/atsuishio/superbwarfare/datagen/ModItemModelProvider.kt
+++ src/main/kotlin/com/atsuishio/superbwarfare/datagen/ModItemModelProvider.kt
@@ -334,6 +334,7 @@ class ModItemModelProvider(private val output: PackOutput) : DataProvider {
         simpleItem(ModItems.MAGAZINE_EXTEND_PRO)
         simpleItem(ModItems.LOUDSPEAKER)
         simpleItem(ModItems.VERTICAL_GRIP)
+        simpleItem(ModItems.GRIP_RECTANGLE_VERTICAL)
         simpleItem(ModItems.STOCK_UBR)
         simpleItem(ModItems.STOCK_PRS)
         simpleItem(ModItems.STOCK_AR_TACTICAL)
