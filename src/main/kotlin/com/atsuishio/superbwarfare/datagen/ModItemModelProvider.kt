958b65c31d59130073c1b5b7068d46a3534e61b0
Atsuishio <842960157@qq.com>
2026-09-16 13:33:41 +0800

    添加俄制步枪制退器，调整配件属性显示

--- src/main/kotlin/com/atsuishio/superbwarfare/datagen/ModItemModelProvider.kt
+++ src/main/kotlin/com/atsuishio/superbwarfare/datagen/ModItemModelProvider.kt
@@ -326,6 +326,7 @@ class ModItemModelProvider(private val output: PackOutput) : DataProvider {
         simpleItem(ModItems.HISSILENCER)
         simpleItem(ModItems.SILAOWUNCER)
         simpleItem(ModItems.RU_SILENCER)
+        simpleItem(ModItems.MUZZLE_BRAKE_RU)
         simpleItem(ModItems.SILENCER_SUPERB_12)
         simpleItem(ModItems.SILENCER_50_CAL)
         simpleItem(ModItems.AR_SILENCER)
