c472904a84afc925e5bc093d64ca53bcbdec078c
Atsuishio <842960157@qq.com>
2026-09-16 15:09:31 +0800

    添加R17补偿器

--- src/main/kotlin/com/atsuishio/superbwarfare/datagen/ModItemModelProvider.kt
+++ src/main/kotlin/com/atsuishio/superbwarfare/datagen/ModItemModelProvider.kt
@@ -332,6 +332,7 @@ class ModItemModelProvider(private val output: PackOutput) : DataProvider {
         simpleItem(ModItems.MUZZLE_BRAKE_RU)
         simpleItem(ModItems.MUZZLE_BRAKE_AR)
         simpleItem(ModItems.MUZZLE_ZENIT_DTK_1)
+        simpleItem(ModItems.MUZZLE_COMPENSATOR_R_17)
         simpleItem(ModItems.SILENCER_SUPERB_12)
         simpleItem(ModItems.SILENCER_50_CAL)
         simpleItem(ModItems.AR_SILENCER)
