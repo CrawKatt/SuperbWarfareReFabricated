8db6988f8fe82a8c54886063a5d243faf178f904
Light_Quanta <zch22248@foxmail.com>
2026-09-18 03:34:46 +0800

    修正部分备弹判定

--- src/main/java/com/atsuishio/superbwarfare/data/gun/AmmoSource.kt
+++ src/main/java/com/atsuishio/superbwarfare/data/gun/AmmoSource.kt
@@ -6,7 +6,6 @@ import com.atsuishio.superbwarfare.capability.api.IItemHandler
 import com.atsuishio.superbwarfare.tools.isSameItemStack
 import kotlinx.serialization.Transient
 import net.minecraft.world.entity.Entity
-import net.minecraft.world.entity.player.Player
 import net.minecraft.world.item.ItemStack
 
 /**
@@ -75,7 +74,7 @@ class AmmoSource {
      * 消耗指定数量（原始数量，不包括虚拟弹药，不考虑 count）
      */
     fun consume(data: GunData, shooter: Entity?, count: Int): Int {
-        if (count <= 0 || shooter is Player && shooter.isCreative) return 0
+        if (count <= 0 || data.hasInfiniteBackupAmmo(shooter)) return 0
         return strategy.consume(data, this, shooter, count)
     }
 
