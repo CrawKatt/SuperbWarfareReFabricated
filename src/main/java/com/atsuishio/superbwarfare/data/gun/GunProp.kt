c5495e3e7df05b298f34c77d7083d325404e22ed
Atsuishio <842960157@qq.com>
2026-09-17 14:26:07 +0800

    添加理塘丁真爱抽的烟弹

--- src/main/java/com/atsuishio/superbwarfare/data/gun/GunProp.kt
+++ src/main/java/com/atsuishio/superbwarfare/data/gun/GunProp.kt
@@ -142,6 +142,9 @@ class GunProp<T, R>(
         val PROJECTILE = complexProp(DefaultGunData::projectile) { it.value }
 
         @JvmField
+        val PROJECTILE_BONE = plainProp(DefaultGunData::projectileBone)
+
+        @JvmField
         val AMMO_COST_PER_SHOOT = plainProp(DefaultGunData::ammoCostPerShoot)
 
         @JvmField
