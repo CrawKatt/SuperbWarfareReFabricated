26a5f925702c330fab63930d6fcdab26f2861135
17146 <1714673995@qq.com>
2026-09-23 03:41:08 +0800

    添加了钢管瞄准镜

--- src/main/kotlin/com/atsuishio/superbwarfare/datagen/ModItemModelProvider.kt
+++ src/main/kotlin/com/atsuishio/superbwarfare/datagen/ModItemModelProvider.kt
@@ -375,6 +375,7 @@ class ModItemModelProvider(private val output: PackOutput) : DataProvider {
         simpleItem(ModItems.SCOPE_PSO_1)
         simpleItem(ModItems.SCOPE_PU)
         simpleItem(ModItems.SCOPE_PK_A)
+        simpleItem(ModItems.STEEL_PIPE_SCOPE)
         simpleItem(ModItems.TRIANGULAR_MUZZLE)
         simpleItem(ModItems.LINEAR_MUZZLE)
         simpleItem(ModItems.CROSS_MUZZLE)
