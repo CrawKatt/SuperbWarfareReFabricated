7a01a91a73e51523e53e5fd883af167513154ed1
Atsuishio <842960157@qq.com>
2026-09-25 20:21:42 +0800

    添加6kh2刺刀，调整肘击基础伤害

--- src/main/kotlin/com/atsuishio/superbwarfare/datagen/ModItemModelProvider.kt
+++ src/main/kotlin/com/atsuishio/superbwarfare/datagen/ModItemModelProvider.kt
@@ -388,6 +388,7 @@ class ModItemModelProvider(private val output: PackOutput) : DataProvider {
         simpleItem(ModItems.STEEL_PIPE_SILENCER)
         simpleItem(ModItems.STEEL_PIPE_GRIP)
         simpleItem(ModItems.BAYONET_M_9)
+        simpleItem(ModItems.BAYONET_6KH2)
 
         // blocks
         evenSimplerBlockItem(ModBlocks.BARBED_WIRE)
