d65e976bbed2bc69a7b8358906e25e284b5bbf6a
Atsuishio <842960157@qq.com>
2026-09-26 16:02:50 +0800

    添加G-25榴弹发射器模型

--- src/main/kotlin/com/atsuishio/superbwarfare/datagen/ModItemModelProvider.kt
+++ src/main/kotlin/com/atsuishio/superbwarfare/datagen/ModItemModelProvider.kt
@@ -392,8 +392,6 @@ class ModItemModelProvider(private val output: PackOutput) : DataProvider {
         simpleItem(ModItems.STEEL_PIPE_GRIP)
         simpleItem(ModItems.BAYONET_M_9)
         simpleItem(ModItems.BAYONET_6KH2)
-
-        // sub weapons（副武器：复用钢管枪口配件的模型与贴图，模型做好后再换）
         simpleItem(ModItems.GP_25)
 
         // blocks
